03de7b3df259da9c320ddb2a073c99100e510dd7
blausues <blausues@gmail.com>
2018-01-10 17:35:38 +0900

    .

--- app/src/main/java/com/example/student/diary_project/MainMonthActivity.java
+++ app/src/main/java/com/example/student/diary_project/MainMonthActivity.java
@@ -1,8 +1,17 @@
 package com.example.student.diary_project;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by student on 2018-01-10.
  */
 
-public class MainMonthActivity {
+public class MainMonthActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_month);
+    }
 }
