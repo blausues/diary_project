2dc9f9d5891deb65d59e40403de9f72912e3188b
dhks223 <dhks223@naver.com>
2018-01-10 17:26:15 +0900

    작성

--- app/src/main/java/com/example/student/diary_project/MainActivity.java
+++ app/src/main/java/com/example/student/diary_project/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.student.diary_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton btnThema, btnWrite, btnSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_month);
+        setContentView(R.layout.activity_day);
+
+        btnThema = findViewById(R.id.btn_thema);
+        btnWrite = findViewById(R.id.btn_write);
+        btnSetting = findViewById(R.id.btn_setting);
+
+        btnThema.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"dsad",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
