4a8fb1bca2f3171ca38ac5a647c40ae9c0f0706b
blausues <blausues@gmail.com>
2018-01-10 17:20:15 +0900

    .

--- app/src/main/java/com/example/student/diary_project/MainActivity.java
+++ app/src/main/java/com/example/student/diary_project/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_month);
+        setContentView(R.layout.activity_write_diet);
     }
 }
