003848a4880412faa8ce2731d412c690dc2298a8
dhks223 <dhks223@naver.com>
2018-01-11 09:17:59 +0900

    하단부 padding 수정

--- app/src/main/java/com/example/student/diary_project/MainActivity.java
+++ app/src/main/java/com/example/student/diary_project/MainActivity.java
@@ -3,10 +3,11 @@ package com.example.student.diary_project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnCalender;
     private ImageButton btnThema, btnWrite, btnSetting;
 
     @Override
@@ -14,21 +15,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day);
 
+        btnCalender = findViewById(R.id.btn_calender);
         btnThema = findViewById(R.id.btn_thema);
         btnWrite = findViewById(R.id.btn_write);
         btnSetting = findViewById(R.id.btn_setting);
 
+        btnCalender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         btnThema.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"dsad",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
 
         btnSetting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"dsad",Toast.LENGTH_SHORT).show();
             }
         });
     }
