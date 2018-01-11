5026b28e9f4044ed41dbaca3dc2bb39a4fefc70e
dhks223 <dhks223@naver.com>
2018-01-11 14:01:27 +0900

    ====

--- app/src/main/java/com/example/student/diary_project/MainActivity.java
+++ app/src/main/java/com/example/student/diary_project/MainActivity.java
@@ -63,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
 
                         btnCalendar.setText(year+"."+month+"."+day);
 
+
+
                         dialog.cancel();
                     }
                 });
