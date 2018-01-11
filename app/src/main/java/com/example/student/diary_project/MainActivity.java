3aed24956175df498fba9d32a0773042eb7215e8
dhks223 <dhks223@naver.com>
2018-01-11 18:49:42 +0900

    일정선택 수정

--- app/src/main/java/com/example/student/diary_project/MainActivity.java
+++ app/src/main/java/com/example/student/diary_project/MainActivity.java
@@ -23,9 +23,12 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private TextView viewDate;
-    private ImageButton btnCalendar,btnThema, btnWrite, btnSetting;
-    private Button btnCalendarDay,btnCalendarMonth,btnCalendarYear;
+    private ImageButton btnCalendar, btnThema, btnWrite, btnSetting;
+    private Button btnCalendarDay, btnCalendarMonth, btnCalendarYear;
+    private Button btnMonthDate;
+    private ImageButton btnMonthLeft, btnMonthRight;
     private MaterialCalendarView materialCalendarView;
+    private String year, month, day;
 
 
     @Override
@@ -42,10 +45,10 @@ public class MainActivity extends AppCompatActivity {
         SimpleDateFormat yearSdf = new SimpleDateFormat("yyyy");
         SimpleDateFormat monthSdf = new SimpleDateFormat("MM");
 
-        String year = yearSdf.format(new Date());
-        String month = monthSdf.format(new Date());
+        year = yearSdf.format(new Date());
+        month = monthSdf.format(new Date());
 
-        viewDate.setText(year+"."+month);
+        viewDate.setText(year + "." + month);
 
         btnCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
                         dialogDay.setContentView(R.layout.dialog_calendar_day);
 
                         //start 달력 데코 (주말 색 표시,오늘 날짜 색 표시)
-                        materialCalendarView = (MaterialCalendarView)dialogDay.findViewById(R.id.dialog_calendar);
+                        materialCalendarView = (MaterialCalendarView) dialogDay.findViewById(R.id.dialog_calendar);
                         materialCalendarView.addDecorators(
                                 new CalendarSaturdayDecorate(),
                                 new CalendarSundayDecorate(),
@@ -83,11 +86,11 @@ public class MainActivity extends AppCompatActivity {
                                 SimpleDateFormat daySdf = new SimpleDateFormat("dd");
 
                                 Date mDate = date.getDate();
-                                String year = yearSdf.format(mDate);
-                                String month = monthSdf.format(mDate);
-                                String day = daySdf.format(mDate);
+                                year = yearSdf.format(mDate);
+                                month = monthSdf.format(mDate);
+                                day = daySdf.format(mDate);
 
-                                viewDate.setText(year+"."+month+"."+day);
+                                viewDate.setText(year + "." + month + "." + day);
 
                                 dialogDay.cancel();
                             }
@@ -97,7 +100,92 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
                 ///////////////////////////////////////////////////////////////////////
+                //월별
+                btnCalendarMonth.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final Dialog dialogMonth = new Dialog(v.getContext());
+                        dialogMonth.setContentView(R.layout.dialog_calendar_month);
 
+                        btnMonthLeft = dialogMonth.findViewById(R.id.btn_month_left);
+                        btnMonthDate = dialogMonth.findViewById(R.id.btn_month_date);
+                        btnMonthRight = dialogMonth.findViewById(R.id.btn_month_right);
+
+                        SimpleDateFormat yearDialogMonth = new SimpleDateFormat("yyyy");
+                        SimpleDateFormat monthDialogMonth = new SimpleDateFormat("MM");
+
+                        year = yearDialogMonth.format(new Date());
+                        month = monthDialogMonth.format(new Date());
+
+                        btnMonthDate.setText(year + " . " + month);
+
+                        btnMonthLeft.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                int tMonth = Integer.parseInt(month);
+                                int tYear = Integer.parseInt(year);
+                                if (tMonth == 1) {
+                                    tYear--;
+                                    tMonth = 12;
+                                } else if (tMonth == 12) {
+                                    tMonth = 11;
+                                } else {
+                                    tMonth = (tMonth) % 12 - 1;
+                                }
+                                if (tMonth < 10) {
+                                    month = "0" + Integer.toString(tMonth);
+                                } else {
+                                    month = Integer.toString(tMonth);
+                                }
+
+                                year = Integer.toString(tYear);
+                                btnMonthDate.setText(year + " . " + month);
+                            }
+                        });
+
+                        btnMonthDate.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                viewDate.setText(year + "." + month);
+                                dialogMonth.cancel();
+                            }
+                        });
+
+                        btnMonthRight.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                int tMonth = Integer.parseInt(month);
+                                int tYear = Integer.parseInt(year);
+                                if (tMonth == 12) {
+                                    tYear++;
+                                }
+                                tMonth = (tMonth) % 12 + 1;
+                                month = Integer.toString(tMonth);
+                                year = Integer.toString(tYear);
+
+                                if (tMonth < 10) {
+                                    month = "0" + Integer.toString(tMonth);
+                                } else {
+                                    month = Integer.toString(tMonth);
+                                }
+
+                                year = Integer.toString(tYear);
+                                btnMonthDate.setText(year + " . " + month);
+                            }
+                        });
+
+
+                        WindowManager.LayoutParams lp1 = new WindowManager.LayoutParams();
+                        lp1.copyFrom(dialogMonth.getWindow().getAttributes());
+                        lp1.width = 950;
+                        lp1.height = 400;
+                        dialogMonth.show();
+                        Window window = dialogMonth.getWindow();
+                        window.setAttributes(lp1);
+                        dialog.cancel();
+                    }
+                });
+                /////////////////////////////////////////////////////////////////////
 
                 WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
                 lp.copyFrom(dialog.getWindow().getAttributes());
