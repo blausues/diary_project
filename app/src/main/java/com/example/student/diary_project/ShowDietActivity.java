27887693bb67aff5fd05a334b833d02ef2ff4fec
blausues <blausues@gmail.com>
2018-01-23 17:46:34 +0900

    .

---
+++ app/src/main/java/com/example/student/diary_project/ShowDietActivity.java
@@ -0,0 +1,16 @@
+package com.example.student.diary_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by student on 2018-01-23.
+ */
+
+public class ShowDietActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
